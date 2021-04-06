2cd038f21eab164bb8b17263767a53a7bb09a23c
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-06 17:36:16 +0900

    投稿処理の追加

--- redux-essentials-example-app/src/features/posts/AddPostForm.js
+++ redux-essentials-example-app/src/features/posts/AddPostForm.js
@@ -1,11 +1,33 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { postAdded } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
+  const dispatch = useDispatch()
+
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        })
+      )
+
+      //stateの更新。フォームの中身を空にする
+      setTitle('')
+      setContent('')
+    }
+  }
 
   return (
     <section>
@@ -26,7 +48,9 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button">Save Post</button>
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
       </form>
     </section>
   )
