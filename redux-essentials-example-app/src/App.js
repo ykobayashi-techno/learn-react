47293f4beed13420e22fde95908e65aee8a8dc9a
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-09 18:42:38 +0900

    編集用のRouteを追加

--- redux-essentials-example-app/src/App.js
+++ redux-essentials-example-app/src/App.js
@@ -8,6 +8,7 @@ import {
 
 import { Navbar } from './app/Navbar'
 import { AddPostForm } from './features/posts/AddPostForm'
+import { EditPostForm } from './features/posts/EditPostForm'
 import { PostsList } from './features/posts/postsList'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 
@@ -28,6 +29,7 @@ function App() {
             )}
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
