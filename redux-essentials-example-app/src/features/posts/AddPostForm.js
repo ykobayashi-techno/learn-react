db6a96f8667043530f6c75af80b8d89836d0bb35
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-09 18:22:14 +0900

    idの生成が不必要になったため削除

--- redux-essentials-example-app/src/features/posts/AddPostForm.js
+++ redux-essentials-example-app/src/features/posts/AddPostForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
 
 import { postAdded } from './postsSlice'
 
@@ -17,7 +16,6 @@ export const AddPostForm = () => {
     if (title && content) {
       dispatch(
         postAdded({
-          id: nanoid(),
           title,
           content,
         })
