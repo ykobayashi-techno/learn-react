a8d12249da0a67454a483d77a29bd2738377f13e
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-09 19:29:17 +0900

    ユーザー名の表示

--- redux-essentials-example-app/src/features/posts/SinglePostPage.js
+++ redux-essentials-example-app/src/features/posts/SinglePostPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
 
 export const SinglePostPage = ({ match }) => {
-  const { postId } = match.params
+  const { postId, userId } = match.params
 
   const post = useSelector((state) =>
     state.posts.find((post) => post.id === postId)
@@ -22,6 +23,10 @@ export const SinglePostPage = ({ match }) => {
       <article className="post">
         <h2>{post.title}</h2>
         <p className="post-content">{post.content}</p>
+
+        <section>
+          <PostAuthor userId={post.userId}></PostAuthor>
+        </section>
 
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
--- redux-essentials-example-app/src/features/posts/postsList.js
+++ redux-essentials-example-app/src/features/posts/postsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
@@ -9,6 +10,7 @@ export const PostsList = () => {
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <PostAuthor userId={post.userId}></PostAuthor>
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
