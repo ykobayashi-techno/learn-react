4ecedc790d053ed265b3085cfa2b68d8335564af
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-10 12:29:50 +0900

    投稿時間の表示

--- redux-essentials-example-app/src/features/posts/SinglePostPage.js
+++ redux-essentials-example-app/src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -22,12 +23,12 @@ export const SinglePostPage = ({ match }) => {
     <section>
       <article className="post">
         <h2>{post.title}</h2>
-        <p className="post-content">
-          {post.content}
-          <PostAuthor userId={post.userId}></PostAuthor>
-        </p>
+        <p className="post-content">{post.content}</p>
 
-        <section></section>
+        <div>
+          <PostAuthor userId={post.userId}></PostAuthor>
+          <TimeAgo timestamp={post.date}></TimeAgo>
+        </div>
 
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
--- redux-essentials-example-app/src/features/posts/postsList.js
+++ redux-essentials-example-app/src/features/posts/postsList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
@@ -13,11 +14,12 @@ export const PostsList = () => {
   const renderedPosts = orderedPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
-      <p className="post-content">
-        {post.content.substring(0, 100)}
-        <br />
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <div>
         <PostAuthor userId={post.userId}></PostAuthor>
-      </p>
+        <TimeAgo timestamp={post.date}></TimeAgo>
+      </div>
 
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
