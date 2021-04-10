c3f70d404d8b6875dc0b30bac5dced99499b1680
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-10 14:45:37 +0900

    関数を外側に移動

--- redux-essentials-example-app/src/features/posts/ReactionButtons.js
+++ redux-essentials-example-app/src/features/posts/ReactionButtons.js
@@ -14,15 +14,19 @@ const reactionEmoji = {
 export const ReactionButtons = ({ post }) => {
   const dispatch = useDispatch()
 
+  const onAddReaction = (postId, reaction) => {
+    if (postId && reaction) {
+      dispatch(reactionAdded({ postId, reaction }))
+    }
+  }
+
   const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     return (
       <button
         key={name}
         type="button"
         className="muted-button reaction-button"
-        onClick={() =>
-          dispatch(reactionAdded({ postId: post.id, reaction: name }))
-        }
+        onClick={() => onAddReaction(post.id, name)}
       >
         {emoji} {post.reactions[name]}
       </button>
