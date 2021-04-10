f2183f55a0aa875ca46cffba6c384c1076f60818
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-10 14:21:47 +0900

    リアクションボタンの表示

--- redux-essentials-example-app/src/features/posts/SinglePostPage.js
+++ redux-essentials-example-app/src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
@@ -29,6 +30,8 @@ export const SinglePostPage = ({ match }) => {
           <PostAuthor userId={post.userId}></PostAuthor>
           <TimeAgo timestamp={post.date}></TimeAgo>
         </div>
+
+        <ReactionButtons post={post}></ReactionButtons>
 
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
