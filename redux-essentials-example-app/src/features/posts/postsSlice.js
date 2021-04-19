5f8bd59930d1e066154ea3b5709b9863c4151261
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-19 14:50:56 +0900

    サーバー保存処理追加

--- redux-essentials-example-app/src/features/posts/postsSlice.js
+++ redux-essentials-example-app/src/features/posts/postsSlice.js
@@ -12,6 +12,17 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   return response.posts
 })
 
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  // The payload creator receives the partial `{title, content, user}` object
+  async (initialPost) => {
+    // We send the initial data to the fake API server
+    const response = await client.post('/fakeApi/posts', { post: initialPost })
+    // The response includes the complete post object, including unique ID
+    return response.post
+  }
+)
+
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
@@ -67,6 +78,10 @@ const postsSlice = createSlice({
     [fetchPosts.rejected]: (state, action) => {
       state.status = 'failed'
       state.error = action.error.message
+    },
+    [addNewPost.fulfilled]: (state, action) => {
+      // We can directly add the new post object to our posts array
+      state.posts.push(action.payload)
     },
   },
 })
