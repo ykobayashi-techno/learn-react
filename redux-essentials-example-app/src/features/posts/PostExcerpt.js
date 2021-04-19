e1334d016e73ac21495a50f39c29dfce764bfe12
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-19 18:55:13 +0900

    EntityAdapterを使用した処理に変更

--- redux-essentials-example-app/src/features/posts/PostExcerpt.js
+++ redux-essentials-example-app/src/features/posts/PostExcerpt.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { selectPostById } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 
-export const PostExcerpt = ({ post }) => {
+export const PostExcerpt = ({ postId }) => {
+  const post = useSelector((state) => selectPostById(state, postId))
+
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
--- redux-essentials-example-app/src/features/posts/postsList.js
+++ redux-essentials-example-app/src/features/posts/postsList.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { selectAllPosts, fetchPosts } from './postsSlice'
+import { selectAllPosts, fetchPosts, selectPostIds } from './postsSlice'
 
 import { PostExcerpt } from './PostExcerpt'
 
 export const PostsList = () => {
   const dispatch = useDispatch()
-  const posts = useSelector(selectAllPosts)
+  const orderedPostIds = useSelector(selectPostIds)
 
   const postStatus = useSelector((state) => state.posts.status)
   const error = useSelector((state) => state.posts.error)
@@ -23,12 +23,8 @@ export const PostsList = () => {
     content = <div className="loader">Loading...</div>
   } else if (postStatus === 'succeeded') {
     // Sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts
-      .slice()
-      .sort((a, b) => b.date.localeCompare(a.date))
-
-    content = orderedPosts.map((post) => (
-      <PostExcerpt key={post.id} post={post} />
+    content = orderedPostIds.map((postId) => (
+      <PostExcerpt key={postId} postId={postId} />
     ))
   } else if (postStatus === 'failed') {
     content = <div>{error}</div>
