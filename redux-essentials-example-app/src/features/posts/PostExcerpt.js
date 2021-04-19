04c316adebd0d8563ac747ac9eab3aad96d015fa
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-19 18:05:53 +0900

    チュートリアルの為にリアクションボタンを追加しレイアウトを調整

--- redux-essentials-example-app/src/features/posts/PostExcerpt.js
+++ redux-essentials-example-app/src/features/posts/PostExcerpt.js
@@ -2,18 +2,21 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 
 export const PostExcerpt = ({ post }) => {
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-
       <div>
         <PostAuthor userId={post.user}></PostAuthor>
         <TimeAgo timestamp={post.date}></TimeAgo>
       </div>
+
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
 
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
