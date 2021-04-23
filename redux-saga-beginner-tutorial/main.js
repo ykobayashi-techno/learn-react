0ca92d95ff3fce6d5977bf3c900bce501c58e614
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-23 17:27:35 +0900

    prettierによるコード整形

--- redux-saga-beginner-tutorial/main.js
+++ redux-saga-beginner-tutorial/main.js
@@ -1,25 +1,26 @@
-import "babel-polyfill"
+import "babel-polyfill";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
 
-import Counter from './Counter'
-import reducer from './reducers'
+import Counter from "./Counter";
+import reducer from "./reducers";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
-const action = type => store.dispatch({type})
+const action = (type) => store.dispatch({ type });
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
-  )
+      onIncrement={() => action("INCREMENT")}
+      onDecrement={() => action("DECREMENT")}
+    />,
+    document.getElementById("root")
+  );
 }
 
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
