6a67a567416458e7154d4b68ab592dce710b597a
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-04-26 16:10:23 +0900

    sagaの初期設定とボタンのアクション処理

--- redux-saga-beginner-tutorial/main.js
+++ redux-saga-beginner-tutorial/main.js
@@ -3,17 +3,17 @@ import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
-import { createSagaMiddleware } from "redux-saga";
+import createSagaMiddleware from "redux-saga";
 
 import Counter from "./Counter";
 import reducer from "./reducers";
-import { helloSaga } from "./sagas";
+import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
-sagaMiddleware.run(helloSaga);
+sagaMiddleware.run(rootSaga);
 
 const action = (type) => store.dispatch({ type });
 
@@ -23,6 +23,7 @@ function render() {
       value={store.getState()}
       onIncrement={() => action("INCREMENT")}
       onDecrement={() => action("DECREMENT")}
+      onIncrementAsync={() => action("INCREMENT_ASYNC")}
     />,
     document.getElementById("root")
   );
